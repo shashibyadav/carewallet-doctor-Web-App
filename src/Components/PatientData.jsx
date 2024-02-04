5e2848fc73d01eae0b782427b8d6018706cca767
GORLEABHILASH <36183586+GORLEABHILASH@users.noreply.github.com>
2024-02-04 17:48:36 -0500

    patientdata page changes

--- src/Components/PatientData.jsx
+++ src/Components/PatientData.jsx
@@ -1,7 +1,4 @@
 import * as React from 'react';
-import TextField from '@mui/material/TextField';
-import CentreRectangle2 from '../shared/CentreRectangle2';
-import Grid from '@mui/material/Grid';
 import { useSelector, useDispatch } from 'react-redux';
 import '../shared/button.css';
 import '../shared/shared.css';
@@ -9,16 +6,18 @@ import '../shared/buttonTypeOne.css';
 import { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { setPatientDataCriteria as setCriteriaAction } from '../ReduxStore/Slices/Search/patientSlice';
-import { createTheme, ThemeProvider, styled } from '@mui/material/styles';
+
 import { brown } from '@mui/material/colors';
 import patientimg from '../images/patient_img.png';
 
 import copyicon from '../images/copy_icon.png';
 import GreenCheckmark from '../images/GreenCheckmark.png';
+import ID from '../images/license.png';
 import ButtonTypeOne from '../shared/buttonTypeOne';
-import InputField from "../shared/inputField";
 import CentreRectangle from '../shared/CentreRectangle';
 import "../styles/patient-data/patient-data.css";
+import LogoFooter from "./shared/LogoFooter";
+import ButtonPatientSearch from "./shared/ButtonPatientSearch";
 
 const accent = brown['400'];
 
@@ -29,7 +28,7 @@ const inputProps = {
   },
 };
 
-const whiteTheme = createTheme({ palette: { primary: { main: accent } } })
+
 
 const PatientList = () => {
   const criteria = useSelector((state) => state.loginState.searchCriteria);
@@ -47,6 +46,7 @@ const PatientList = () => {
   const [policyNumber, setpolicyNumber] = useState(criteria.policyNumber);
   const [groupNumber, setgroupNumber] = useState(criteria.groupNumber);
   const [policyHDOB, setpolicyHDOB] = useState(criteria.policyHDOB);
+  const [showImage, setShowImage] = useState(false);
   const dispatch = useDispatch();
   const navigate = useNavigate();
   const updateState = (newCriteria) => {
@@ -62,6 +62,17 @@ const PatientList = () => {
     navigate('/PatientData');
   };
 
+  const handleViewId = () => {
+    // Toggle the visibility of the image
+    setShowImage(!showImage);
+  };
+
+
+  const handleCloseImage = () => {
+    setShowImage(false);
+  };
+
+
   return (
     <div style={{ width: '100vw', height: '100vh', backgroundColor: '#1C1C1D' }}>
       <CentreRectangle className='center-rectangle-2'>
@@ -72,7 +83,7 @@ const PatientList = () => {
               Patient: <span style={{ fontSize: '1.5rem', fontWeight: '700' }}>Evan Smith</span>
             </h3>
 
-            <img src={patientimg} alt="Care Wallet Logo" style={{ height: '40%', width: '40%', margin: '10px' }} />
+            <img src={patientimg} alt="Care Wallet Logo" style={{ height: '40%', width: '40%', margin: '10px', marginBottom: '2rem' }} />
             <div className='input-container'>
               <img src={copyicon} alt="Copy Icon" className="copy-icon" />
               <input
@@ -93,141 +104,168 @@ const PatientList = () => {
             </div>
           </div>
           <div className='data-grid-2'  >
-            <div className='input-container'>
-              <img src={copyicon} alt="Copy Icon" className="copy-icon" />
-              <input
-                className='patientdata-search-fields'
-                value={firstName}
-                placeholder="First Name"
-                onChange={(e) => setFirstName(e.target.value)}
-              />
-            </div>
+            <div className='data-grid-2-container'  >
+              <div className='input-container'>
+                <img src={copyicon} alt="Copy Icon" className="copy-icon" />
+                <input
+                  className='patientdata-search-fields patientdata-firstname'
+                  value={firstName}
+                  placeholder="First Name"
+                  onChange={(e) => setFirstName(e.target.value)}
+                />
+              </div>
 
-            <div className='input-container'>
-              <img src={copyicon} alt="Copy Icon" className="copy-icon" />
-              <input
-                className='patientdata-search-fields'
-                value={lastName}
-                placeholder="Last Name"
-                onChange={(e) => setLastName(e.target.value)}
-              />
-            </div>
-
-        
-
-            <div className='input-container'>
-              <img src={copyicon} alt="Copy Icon" className="copy-icon" />
-              <input
-                className='patientdata-search-fields'
-                value={address}
-                placeholder="Address"
-                onChange={(e) => setAddress(e.target.value)}
-              />
-            </div>
-
-            <div className='input-container'>
-              <img src={copyicon} alt="Copy Icon" className="copy-icon" />
-              <input
-                className='patientdata-search-fields'
-                value={city}
-                placeholder="City"
-                onChange={(e) => setCity(e.target.value)}
-              />
-            </div>
-
-            <div className='input-container'>
-              <img src={copyicon} alt="Copy Icon" className="copy-icon" />
-              <input
-                className='patientdata-search-fields'
-                value={state}
-                placeholder="State"
-                onChange={(e) => setstate(e.target.value)}
-              />
-            </div>
+              <div className='input-container'>
+                <img src={copyicon} alt="Copy Icon" className="copy-icon" />
+                <input
+                  className='patientdata-search-fields'
+                  value={lastName}
+                  placeholder="Last Name"
+                  onChange={(e) => setLastName(e.target.value)}
+                />
+              </div>
 
 
 
-            <div className='input-container'>
-              <img src={copyicon} alt="Copy Icon" className="copy-icon" />
-              <input
-                className='patientdata-search-fields'
-                value={zipcode}
-                placeholder="Zip Code"
-                onChange={(e) => setZipcode(e.target.value)}
-              />
-            </div>
+              <div className='input-container'>
+                <img src={copyicon} alt="Copy Icon" className="copy-icon" />
+                <input
+                  className='patientdata-search-fields'
+                  value={address}
+                  placeholder="Address"
+                  onChange={(e) => setAddress(e.target.value)}
+                />
+              </div>
+
+              <div className='input-container'>
+                <img src={copyicon} alt="Copy Icon" className="copy-icon" />
+                <input
+                  className='patientdata-search-fields'
+                  value={city}
+                  placeholder="City"
+                  onChange={(e) => setCity(e.target.value)}
+                />
+              </div>
+
+              <div className='input-container'>
+                <img src={copyicon} alt="Copy Icon" className="copy-icon" />
+                <input
+                  className='patientdata-search-fields'
+                  value={state}
+                  placeholder="State"
+                  onChange={(e) => setstate(e.target.value)}
+                />
+              </div>
 
 
 
-            <div className="search-action">
-              <ButtonTypeOne text='View ID' className='button-type-one'  ></ButtonTypeOne>
+              <div className='input-container'>
+                <img src={copyicon} alt="Copy Icon" className="copy-icon" />
+                <input
+                  className='patientdata-search-fields'
+                  value={zipcode}
+                  placeholder="Zip Code"
+                  onChange={(e) => setZipcode(e.target.value)}
+                />
+              </div>
 
+
+
+              <div className="search-action">
+                <ButtonTypeOne text='View ID' className='button-type-one' onClick={handleViewId} ></ButtonTypeOne>
+
+              </div>
             </div>
           </div>
           <div className='data-grid-3'  >
-            <div style={{ display: 'flex' }}><img src={GreenCheckmark} alt="GreenCheckmark" style={{ height: '20%', width: '20%', margin: '10px' }} /><h3 style={{ color: 'green' , width:'10px'}}>Active:01/24/2024</h3></div>
-            <div className='input-container'>
-              <img src={copyicon} alt="Copy Icon" className="copy-icon" />
-              <input
-                className='patientdata-search-fields'
-                value={insuranceName}
-                placeholder="Insurance Name"
-                onChange={(e) => setinsuranceName(e.target.value)}
-              />
+            <div className='data-grid-3-container'  >
+              {showImage && (
+                <>
+                  <div className="user-id-image-container">
+                    <img src={ID} alt="Your Image" className="user-id-image" />
+                  </div>
+                </>
+              )}
+
+              {!showImage && (
+                <>
+
+                  <div className='status-container'>
+
+                    <img src={GreenCheckmark} alt="Activity Icon" className="active-icon" />
+                    <div className="active-status" ><h3 >Active:01/24/2024</h3></div>
+
+                  </div>
+
+                  <div className='input-container'>
+                    <img src={copyicon} alt="Copy Icon" className="copy-icon" />
+                    <input
+                      className='patientdata-search-fields'
+                      value={insuranceName}
+                      placeholder="Insurance Name"
+                      onChange={(e) => setinsuranceName(e.target.value)}
+                    />
+                  </div>
+
+                  <div className='input-container'>
+                    <img src={copyicon} alt="Copy Icon" className="copy-icon" />
+                    <input
+                      className='patientdata-search-fields'
+                      value={policyHName}
+                      placeholder="Policyholder Name"
+                      onChange={(e) => setpolicyHName(e.target.value)}
+                    />
+                  </div>
+
+                  <div className='input-container'>
+                    <img src={copyicon} alt="Copy Icon" className="copy-icon" />
+                    <input
+                      className='patientdata-search-fields'
+                      value={policyNumber}
+                      placeholder="Policy #"
+                      onChange={(e) => setpolicyNumber(e.target.value)}
+                    />
+                  </div>
+
+                  <div className='input-container'>
+                    <img src={copyicon} alt="Copy Icon" className="copy-icon" />
+                    <input
+                      className='patientdata-search-fields'
+                      value={groupNumber}
+                      placeholder="Group #"
+                      onChange={(e) => setgroupNumber(e.target.value)}
+                    />
+                  </div>
+
+                  <div className='input-container'>
+                    <img src={copyicon} alt="Copy Icon" className="copy-icon" />
+                    <input
+                      className='patientdata-search-fields'
+                      value={policyHDOB}
+                      placeholder="Policyholder DOB"
+                      onChange={(e) => setpolicyHDOB(e.target.value)}
+                    />
+                  </div>
+
+
+
+                  <div className="search-action">
+                    <ButtonTypeOne text='View Insurance' className='patientdata-button-type-one'  ></ButtonTypeOne>
+                  </div>
+                  <div className="search-action">
+                    <ButtonTypeOne text='View Activity Log' className='patientdata-button-type-one'  ></ButtonTypeOne>
+                  </div>
+                </>
+              )}
             </div>
 
-            <div className='input-container'>
-              <img src={copyicon} alt="Copy Icon" className="copy-icon" />
-              <input
-                className='patientdata-search-fields'
-                value={policyHName}
-                placeholder="Policyholder Name"
-                onChange={(e) => setpolicyHName(e.target.value)}
-              />
-            </div>
-
-            <div className='input-container'>
-              <img src={copyicon} alt="Copy Icon" className="copy-icon" />
-              <input
-                className='patientdata-search-fields'
-                value={policyNumber}
-                placeholder="Policy #"
-                onChange={(e) => setpolicyNumber(e.target.value)}
-              />
-            </div>
-
-            <div className='input-container'>
-              <img src={copyicon} alt="Copy Icon" className="copy-icon" />
-              <input
-                className='patientdata-search-fields'
-                value={groupNumber}
-                placeholder="Group #"
-                onChange={(e) => setgroupNumber(e.target.value)}
-              />
-            </div>
-
-            <div className='input-container'>
-              <img src={copyicon} alt="Copy Icon" className="copy-icon" />
-              <input
-                className='patientdata-search-fields'
-                value={policyHDOB}
-                placeholder="Policyholder DOB"
-                onChange={(e) => setpolicyHDOB(e.target.value)}
-              />
-            </div>
-
-
-           
-            <div className="search-action">
-              <ButtonTypeOne text='View Insurance' className='button-type-one'  ></ButtonTypeOne>
-            </div>
-            <div className="search-action">
-              <ButtonTypeOne text='View Activity Log' className='button-type-one'  ></ButtonTypeOne>
-            </div>
           </div>
         </div>
 
       </CentreRectangle>
+      <ButtonPatientSearch classname={`referrals-button`} onClick={() => { }} text={`Referrals`} />
+      <ButtonPatientSearch classname={`search-button`} onClick={() => { }} text={`Patient Search`} />
+      <LogoFooter />
     </div>
   );
 }
