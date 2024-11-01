63760b41d0a88a1614a99f6a32e0137bfc1ef7e3
Tahmid Washy <146319268+washyt@users.noreply.github.com>
2024-11-01 19:59:46 -0400

    homepage slice

--- src/ReduxStore/Setup/Store.js
+++ src/ReduxStore/Setup/Store.js
@@ -3,12 +3,14 @@ import loginSlice from '../Slices/Login/loginSlice'
 import loginCredSlice from "../Slices/Login/loginCredSlice";
 import registerSlice  from '../Slices/Login/registerSlice';
 import locationsSlice from '../Slices/Login/locationSlice';
+import doctorHomePageSlice from '../Slices/Login/homePageSlice';
 
 export const store = configureStore({
   reducer: {
     loginState : loginSlice,
     loginCredState: loginCredSlice,
     registerState: registerSlice,
-    locationsState: locationsSlice
+    locationsState: locationsSlice,
+    homeState: doctorHomePageSlice
   },
 })
